028f86866b2870d1d9347c4551910df41b042060
Peter Barclay <peter@peter-barclay.com>
2019-10-31 14:07:23 +0000

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright © 2019 Peter Barclay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
